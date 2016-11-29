817430a800b1e8cbe9f0b60219d806178f47d2e5
pranav-maheshwari <pranavm.13@gmail.com>
2016-11-29 00:16:13 -0500

    Bars and Bugtrap
    All done

--- sample_environments/environment.py
+++ sample_environments/environment.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from collections import namedtuple
 import random
+import os
 
 _config = namedtuple('_config', 'res_x res_y type count width depth')
 
@@ -11,7 +12,7 @@ _config.type = "trap"       # Map type (trap or bars)
 _config.count = 2           # Number of bars
 _config.width = 32          # Max Width of trap
 _config.depth = 32          # Max Depth of trap
-_config.length = 24         # Length of bars
+_config.length = 32         # Length of bars
 _config.start = [40, 25]    # Start Position
 _config.goal = [40, 50]     # Goal Position
 _config.boundaries_x = sorted([_config.start[1], _config.goal[1]])   # Region occupied by bars or trap
@@ -21,18 +22,20 @@ _config.thickness = 5       # Thickness of walls
 class Example:
     def __init__(self, count, param, disp):
         for i in range(count):
-            f = open(str(i)+_config.type+".txt", 'w')
+            f = open(os.path.join(_config.type,str(i)) + ".txt", 'w')
             buffer_temp = param[0:2]
             if disp:
                 self.output = np.ones([_config.res_x, _config.res_y], dtype=int) * 255
             if _config.type == "bars":
-                """
-                for i in range(0, _config.count):
-                    x = random.randint(0, _config.res_x)
-                    y = random.randint(int((1-_config.span)*_config.res_y), int(_config.span*_config.res_y))
-                    if disp:
-                        self.output = cv2.line(self.output, (x, y - _config.length/2), (x, y + _config.length/2), (0, 0, 0), thickness=4)
-                """
+                walls = list()
+                for k in range(0, _config.count):
+                    length = max(3*_config.thickness, int(random.random()*_config.length))
+                    x = random.randint(_config.boundaries_x[0], _config.boundaries_x[1])
+                    y = random.randint(0, _config.res_y - length)
+                    walls.append([x, y, x + _config.thickness, y + length])
+                if disp:
+                    for k in walls:
+                        cv2.rectangle(self.output, tuple(k[0:2]), tuple(k[2:]), (0, 0, 0), -1)
             elif _config.type == "trap":
                 depth = max(2*_config.thickness, int(random.random()*_config.depth))
                 width = max(2*_config.thickness, int(random.random()*_config.width))
@@ -54,7 +57,7 @@ class Example:
                 self.display(i)
 
     def display(self, count):
-        cv2.imwrite(str(count)+_config.type + ".jpg", self.output)
+        cv2.imwrite(os.path.join(_config.type, str(count)) + ".jpg", self.output)
 
 def static_strings():
     output = list()
