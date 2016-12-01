350fa453bb4d837183b7281e96fcda6edf6a1e31
pranav-maheshwari <pranavm.13@gmail.com>
2016-12-01 13:43:07 -0500

    Fresh Puddles
    Puddles are ready!

--- heuristic_learning/environment_database/environment.py
+++ heuristic_learning/environment_database/environment.py
@@ -10,7 +10,7 @@ _config = namedtuple('_config', 'res_x res_y type count width depth')
 
 _config.res_x = 64          # Resolution in X axis
 _config.res_y = 64          # Resolution in Y axis
-_config.type = "bugtrap_environments"       # Map type (trap or bars)
+_config.type = "puddle"       # Map type (trap or bars or puddle)
 _config.count = 2           # Number of bars
 _config.length = 32         # Length of bars
 _config.start = [40, 25]    # Start Position
@@ -18,6 +18,8 @@ _config.goal = [40, 50]     # Goal Position
 _config.thickness = 5       # Thickness of walls
 _config.depth = abs(_config.goal[1] - _config.start[1] - _config.thickness - 2)             # Max Width of trap
 _config.width = 32          # Max Depth of trap
+_config.puddle_size = 10    # Max puddle size
+_config.puddle_count = 10   # Max number of puddles
 _config.boundaries_x = sorted([_config.start[1], _config.goal[1]])                      # Region occupied by bars or trap
 _config.boundaries_y = sorted([_config.start[0], _config.goal[0]])                      # Region occupied by bars or trap
 
@@ -54,9 +56,25 @@ class Example:
                 if disp:
                     for k in walls:
                         cv2.rectangle(self.output, tuple(k[0:2]), tuple(k[2:]), (0, 0, 0), -1)
+            elif _config.type == "puddle":
+                walls = list()
+                puddle_count = int(random.random() * _config.puddle_count)
+                for k in range(puddle_count):
+                    puddle_size = int(max(0.1, random.random())*_config.puddle_size)
+                    x = random.randint(0, _config.res_x - puddle_size - 1)
+                    y = random.randint(0, _config.res_y - puddle_size - 1)
+                    while not self.clearance_check(x, y, puddle_size):
+                        x = random.randint(0, _config.res_x)
+                        y = random.randint(0, _config.res_y)
+                    walls.append([x, y, x + puddle_size, y + puddle_size])
+                    if disp:
+                        for j in walls:
+                            cv2.rectangle(self.output, tuple(j[0:2]), tuple(j[2:]), (0, 0, 0), -1)
             for k in walls:
                 k = [k[1], k[0], k[3], k[2]]
                 buffer_temp.append(str(k)[1:-1])
+            if _config.type == "puddle":
+                buffer_temp.append("count " + str(puddle_count))
             buffer_temp += param[-2:]
             for k in buffer_temp:
                 f.write(k)
@@ -67,6 +85,12 @@ class Example:
 
     def display(self, count):
         cv2.imwrite(os.path.join(_config.type, str(count)) + ".jpg", self.output)
+
+    def clearance_check(self, x, y, size):
+        if x <= _config.start[1] <= x + size or y <= _config.start[0] <= y + size or x <= _config.goal[1] <= x + size or y <= _config.goal[0] <= y + size:
+            return False
+        else:
+            return True
 
 def static_strings():
     output = list()
